2def85ef2c0e637d539a4bdba31d2514ea6227ed
shukonin <fernando.canas@outlook.com>
2020-10-03 16:42:05 -0500

    First commit

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,15 @@
+function MediaPlayer(config) {
+    this.media = config.el
+}
+
+MediaPlayer.prototype.play = function() {
+    this.media.play()
+}
+MediaPlayer.prototype.pause = function() {
+    this.media.pause()
+}
+MediaPlayer.prototype.togglePlay = function() {
+    (this.media.paused) ? this.play() : this.pause()
+}
+
+export default MediaPlayer
