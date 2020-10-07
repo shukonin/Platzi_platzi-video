daad896d2c99d9058c81439f262a075163d0b0a7
shukonin <fernando.canas@outlook.com>
2020-10-06 23:01:12 -0500

    service workers, visibilitychange e IntersectionObserver

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,13 @@
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector('video')
 const player = new MediaPlayer({
     el: video,
     plugins: [
-        //new AutoPlay()
+        new AutoPlay(),
+        new AutoPause()
     ]
 })
 
@@ -14,3 +16,9 @@ play_btn.onclick = () => player.togglePlay()
 
 const mute_btn = document.getElementById('mute_btn')
 mute_btn.onclick = () => player.toggleMute()
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+        console.log(error.message)
+    })
+}
