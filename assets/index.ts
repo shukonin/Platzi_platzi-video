08eb7d24b7223cf209005b398062ca09431517a4
shukonin <fernando.canas@outlook.com>
2020-10-10 15:49:56 -0500

    ads on console

--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,16 @@
 import MediaPlayer from './MediaPlayer'
 import AutoPlay from './plugins/AutoPlay'
 import AutoPause from './plugins/AutoPause'
+import Ads from './plugins/Ads'
+import AdsPlugin from './plugins/Ads'
 
 const video = document.querySelector('video')
 const player = new MediaPlayer({
     el: video,
     plugins: [
         new AutoPlay(),
-        new AutoPause()
+        new AutoPause(),
+        new AdsPlugin()
     ]
 })
 
